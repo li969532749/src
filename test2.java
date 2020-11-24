8258e463c8c0304c48e5a287ef776857673287d8
li969532749 <1585616618@qq.com>
2020-11-24 21:37:10 +0800

    000

---
+++ test2.java
@@ -0,0 +1,15 @@
+
+public class test2 {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+     int a=100;
+     int b=10;
+     System.out.println("a="+a+"\tb="+b);
+     a=b;
+     b=a;
+     System.out.println("a="+a+"\tb="+b);
+     
+	}
+
+}
